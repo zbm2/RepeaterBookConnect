69aa66789991f713df45570200bad0a49408b33e
Nicolas Jon Pike <nicolas.jon.pike@gmail.com>
2026-07-04 12:12:42 +0100

    tidying imports

--- app/src/main/java/com/zbm2/rbresolver/MainActivity.java
+++ app/src/main/java/com/zbm2/rbresolver/MainActivity.java
@@ -27,8 +27,8 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 import com.google.android.gms.tasks.CancellationTokenSource;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+// import com.google.android.gms.tasks.OnFailureListener;
+// import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
